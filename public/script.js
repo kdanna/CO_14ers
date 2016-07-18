d6365edcfd71dffc99e0288e69b347c806fec8ce
kdanna <kayce.danna@gmail.com>
2016-07-17 21:07:18 -0600

    adding Google Map

--- public/script.js
+++ public/script.js
@@ -1,45 +1,59 @@
-$(function () {
+$(document).on("ready", function() {
+    
+
+
+    // $.get(weekly_quakes_endpoint, function(data){
+            
+    //       console.log(data); // this works  
+    //       var quakeNames = template({quakes: data.features});
+    //       $("#info").append(quakeNames);
+
+
+
+    //       var quakeResults = data.features;
+    //       var coordinates = {};
+
+    //       quakeResults.forEach(function(quake) {
+    //       // console.log(quake.geometry.coordinates[0] + " " + quake.geometry.coordinates[1]);
+    //       var lat = quake.geometry.coordinates[0];
+    //       var lng = quake.geometry.coordinates[1];
+    //       coordinates.lat = lat;
+    //       coordinates.lng = lng;
+    //       console.log(coordinates.lat + " " + coordinates.lng);
+    //       var marker = new google.maps.Marker({
+    //             position: coordinates,
+    //             map: map
+    //         });
+    //     });
+
+    // // });
+
+   //  function initMap() {
+   //      map = new google.maps.Map(document.getElementById('map'), {
+   //        center: {lat: 38.829332, lng: -106.139515},
+   //        zoom: 8
+   //      });
+   //  }
+
+   // initMap();
 
     function initMap() {
+      var myLatLng = {lat: 38.829332, lng: -106.139515};
 
-        var location = new google.maps.LatLng(50.0875726, 14.4189987);
+      // Create a map object and specify the DOM element for display.
+      var map = new google.maps.Map(document.getElementById('map-container'), {
+        center: myLatLng,
+        scrollwheel: false,
+        zoom: 10
+      });
 
-        var mapCanvas = document.getElementById('map');
-        var mapOptions = {
-            center: location,
-            zoom: 16,
-            panControl: false,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        
-        var map = new google.maps.Map(mapCanvas, mapOptions);
-
-        var markerImage = 'marker.png';
-
-        var marker = new google.maps.Marker({
-            position: location,
-            map: map,
-            icon: markerImage
-        });
-
-        var contentString = '<div class="info-window">' +
-                '<h3>Info Window Content</h3>' +
-                '<div class="info-content">' +
-                '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>' +
-                '</div>' +
-                '</div>';
-
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString,
-            maxWidth: 400
-        });
-
-        marker.addListener('click', function () {
-            infowindow.open(map, marker);
-        });
-
-
+      // Create a marker and set its position.
+      var marker = new google.maps.Marker({
+        map: map,
+        position: myLatLng,
+        title: 'Buena Vista'
+      });
     }
-
-    google.maps.event.addDomListener(window, 'load', initMap);
-});
+initMap();
+         
+ });
