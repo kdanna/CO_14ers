df2f1b44d711c982ee1763eaeb9845dbda9bfd68
kdanna <kayce.danna@gmail.com>
2016-07-21 11:23:00 -0600

    working on styling

--- public/script.js
+++ public/script.js
@@ -2,13 +2,14 @@ $(document).on("ready", function() {
 
   function initMap() {
       var myLatLng = {lat: 38.829332, lng: -106.139515};
-
       // Create a map object and specify the DOM element for display.
       var map = new google.maps.Map(document.getElementById('map-container'), {
         center: myLatLng,
         scrollwheel: false,
         zoom: 8
       });
+
+      map.setMapTypeId(google.maps.MapTypeId.TERRAIN);
 
       $.get('/api', function(all_mountains){
         var mountains = JSON.parse(all_mountains);
